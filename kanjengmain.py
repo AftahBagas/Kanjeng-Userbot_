fe6bed8910630c6816a3328916b894b80867886c
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-05 20:32:29 +0700

    Create kanjengmain.py

---
+++ kanjengmain.py
@@ -0,0 +1,6 @@
+# Alfareza
+import os
+
+os.system(
+    "git clone https://github.com/AftahBagas/kanjeng-userbot /root/kanjeng-userbot && mkdir /root/kanjeng-userbot/bin/ && cd /root/kanjeng-userbot/ && chmod +x /usr/local/bin/* && python3 -m kanjeng-userbot"
+)
